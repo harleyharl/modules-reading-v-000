cf4a4831b9959bc6fd81e244a37231ad3e68058e
Harley Rayner <harleymteden@gmail.com>
2018-09-12 16:14:04 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,7 +2,7 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
-  
+
   include Dance
 
   extend MetaDancing
@@ -11,7 +11,6 @@ class Dancer
 
   def initialize(name)
     @name = name
-  end 
-
+  end
 
 end
--- lib/kid.rb
+++ lib/kid.rb
@@ -5,10 +5,12 @@ class Kid
 
   include Dance
 
+  extend MetaDancing
+
   attr_accessor :name
 
   def initialize(name)
     @name = name
   end
 
-end 
+end
